e6c9077bcca4a152a73931b38c90ff7267bad031
Raj <87857226+Axxi3@users.noreply.github.com>
2025-04-06 20:07:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,35 @@
 import './App.css'
-import HeroSection from './Components/Hero Section/HeroSection.tsx'
-
+import AboutSection from './Components/AboutSection'
+import EducationSection from './Components/EducationSection'
+import LegacySection from './Components/FinalSection'
 import Footer from './Components/Footer'
-
-import AboutVaibhav from './Components/about Vaibhav/AboutVaibhav.tsx'
-import ValuesSection from './Components/Value section/ValuesSection.tsx'
-import BiographySection from './Components/Education/BiographySection.tsx'
-import HeroImage from './Components/Aesthetic/HeroImage.tsx'
-import LegacySection from './Components/Legacy/LegacySection.tsx'
+import HeroSection from './Components/HeroSection'
+import Navbar from './Components/Navbar'
+import ThirdSection from './Components/ThirsSection'
 
 function App() {
 
 
   return (
    <>
-     <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
-        rel="stylesheet"
-      />
     <div className="min-h-screen bg-[#FAFAFC]">
-   
-    
+      <Navbar />
+      <div className="">
       <div className="relative">
       <HeroSection />
 
       {/* Spacer so AboutVaibhav doesn't overlap fixed Hero */}
-      <div className="relative z-10 pt-[100vh]">
-        <AboutVaibhav />
+      <div className="relative bg-white z-10">
+      <AboutSection/>
       </div>
     </div>
-
-    <ValuesSection/>
-  <BiographySection/>
-
-  <div className="relative">
-      <HeroImage />
-
-      {/* Spacer so AboutVaibhav doesn't overlap fixed Hero */}
-      <div className="relative z-10 pt-[100vh]">
-        <LegacySection />
-      </div>
-    </div>
-      
+        
+        <EducationSection/>
+        <ThirdSection/>
+        <LegacySection/>
         <Footer/>
       </div>
-    
+    </div>
    </>
   )
 }
